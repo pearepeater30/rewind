aa4bb1b26d7d8ad58e080e2fd97834f6416b244e
Nathan <ng00367@surrey.ac.uk>
2021-08-23 21:45:04 -0700

    Init Project

---
+++ client/src/components/AppHeader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+
+const AppHeader = () => {
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant='h5'>
+        My React App
+      </Typography>
+    </Toolbar>
+  </AppBar>
+}
+
+export default AppHeader
