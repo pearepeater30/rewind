4bec1e67477b4d6bcb11564af3519dd0c12d808a
Nathan Gu <nathancgu@gmail.com>
2021-09-13 10:38:08 -0700

    Added fileReader to the filduploadfield

--- client/src/components/FileUploadField.js
+++ client/src/components/FileUploadField.js
@@ -3,9 +3,22 @@ import {useDropzone} from 'react-dropzone'
 
 function FileUploadField() {
   const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+      // Do whatever you want with the file contents
+        const binaryStr = reader.result
+        console.log(binaryStr)
+      }
+      reader.readAsArrayBuffer(file)  
+    })
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  //const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop, accept: 'audio/*'})
 
   return (
     <div {...getRootProps()}>
