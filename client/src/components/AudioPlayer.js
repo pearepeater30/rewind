1ddd9f3bddfbfb36f359c1d20fe510cd57032612
Nathan <nathancgu@gmail.com>
2021-09-20 15:22:03 -0700

    Added styling for the audio player

--- client/src/components/AudioPlayer.js
+++ client/src/components/AudioPlayer.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import '../styles/AudioPlayer.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlay} from '@fortawesome/free-solid-svg-icons'
-  
+import React, { useState } from "react";
+import "../styles/AudioPlayer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faPause,
+  faBackward,
+  faForward,
+} from "@fortawesome/free-solid-svg-icons";
+
 const AudioPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div>
-      <div class = "info">
-        <p>Audio Player</p>
+      <div className="seek-slider-group">
+        <input type="range" className="seek-slider"/>
       </div>
       <div className="player-controller">
-        <button id="play-icon"><FontAwesomeIcon icon={faPlay}></FontAwesomeIcon></button>
-        <span id="current-time" class="time">0:00</span>
-        <input type="range" id="seek-slider" max="100" value="0" />
-        <span id="duration" class="time">0:00</span>
+        <button><FontAwesomeIcon icon={faBackward} /></ button>
+        <button onClick={togglePlayPause} id="play-icon">
+          {isPlaying ? (
+            <FontAwesomeIcon icon={faPause}></FontAwesomeIcon>
+          ) : (
+            <FontAwesomeIcon icon={faPlay}></FontAwesomeIcon>
+          )}
+        </button>
+        <button><FontAwesomeIcon icon={faForward} /></button>
+        <span id="current-time" class="time">
+          0:00
+        </span>
+        /
+        <span id="duration" class="time">
+          0:00
+        </span>
+        
         <input type="range" id="volume-slider" max="100" value="100" />
       </div>
     </div>
-  )
-  
-}
+  );
+};
 
 export default AudioPlayer;
